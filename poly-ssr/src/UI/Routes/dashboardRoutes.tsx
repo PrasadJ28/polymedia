b4f430f4f34f91d7604b7b3943ca473e707a53e3
prasadj28 <prasadjadhav2895@gmail.com>
2025-11-18 18:49:26 -0500

    removed unnecesarry projects

--- poly-ssr/src/UI/Routes/dashboardRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import DashboardLayout from "../Layouts/DashboardLayout";
-export const dashboardRoutes = {
-    path: "/",
-    element: <DashboardLayout/>,
-    children: [
-    ],
-};
--- poly-ssr/src/UI/Routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { dashboardRoutes } from "./dashboardRoutes.tsx";
-const routes = [
-    dashboardRoutes
-];
-const router = createBrowserRouter(routes);
-
-export default router;
